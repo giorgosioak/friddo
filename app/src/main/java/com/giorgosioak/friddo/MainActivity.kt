e94f8c3407a1177d759bb3e252ba65f6a8d3dd48
George Ioakeimidis <giorgosioak95@gmail.com>
2023-11-27 00:32:18 +0000

    Material theme + Top Bottom Bars

--- app/src/main/java/com/giorgosioak/friddo/MainActivity.kt
+++ app/src/main/java/com/giorgosioak/friddo/MainActivity.kt
@@ -3,31 +3,63 @@ package com.giorgosioak.friddo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.giorgosioak.friddo.ui.theme.FriddoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             FriddoTheme {
                 // A surface container using the 'background' color from the theme
@@ -35,20 +67,92 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FridaStatusCard()
+                    HomePage()
                 }
             }
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage() {
+//    var presses by remember { mutableIntStateOf(0) }
+    var menuExpanded by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = {
+                    Text(
+                        text ="Frida Server Companion",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { menuExpanded = !menuExpanded }) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = "More",
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = menuExpanded,
+                        onDismissRequest = { menuExpanded = false },
+                    ) {
+                        DropdownMenuItem(
+                            text = {
+                                Text("Settings")
+                            },
+                            onClick = { /* TODO */ },
+                        )
+                        DropdownMenuItem(
+                            text = {
+                                Text("About")
+                            },
+                            onClick = { /* TODO */ },
+                        )
+                    }
+                }
+
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Bottom app bar",
+                )
+            }
+        },
+        floatingActionButton = {
+//            FloatingActionButton(onClick = { presses++ }) {
+//                Icon(Icons.Default.Add, contentDescription = "Add")
+//            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            FridaStatusCard()
+        }
+    }
+}
+
 
 @Composable
 fun FridaStatusCard() {
-    Box (
+    Card (
+            colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        ),
         modifier = Modifier
-            .wrapContentSize()
-            .padding(0.dp)
-            .background(Color.LightGray, shape = RoundedCornerShape(4.dp))
+            .fillMaxWidth().padding(12.dp)
     ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -59,17 +163,17 @@ fun FridaStatusCard() {
                     Text(
                         text = "Frida Server: ",
                         fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Right
+                        textAlign = TextAlign.Right, color = MaterialTheme.colorScheme.secondary
                     )
                     Text(
                         text = "Status: ",
                         fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Right
+                        textAlign = TextAlign.Right, color = MaterialTheme.colorScheme.secondary
                     )
                 }
                 Column {
-                    Text(text = "16.1.4 (arm64)")
-                    val active = Text(text = "Active", color = Color.Green)
+                    Text(text = "16.1.4 (arm64)", color = MaterialTheme.colorScheme.tertiary)
+                    val active = Text(text = "Active", color = MaterialTheme.colorScheme.tertiary)
                 }
             }
         }
