bd4f1e9578bb2788989f4564058171e3b07faaae
George Ioakeimidis <giorgosioak95@gmail.com>
2026-05-08 13:01:10 +0100

    v1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.giorgosioak.friddo"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
